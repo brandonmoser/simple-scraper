1a68d371ea3d440ccc985f1d375980390987592c
Nick Andren <nick@trackif.com>
2015-02-23 16:06:08 -0600

    Fixed whitespace formatting

--- crawler.php
+++ crawler.php
@@ -28,9 +28,9 @@ for($page=0; $page<count($urlList) && $page<MAX_PAGES; $page++) {
                 $addedLinks++;
             }
         }
-    if (preg_match('@<title>([^<]+)</title>@i', $content, $match) !=0) {
+        if (preg_match('@<title>([^<]+)</title>@i', $content, $match) !=0) {
            $newTitle = $match[1];      
-            }
+        }
 
         echo $addedLinks, " new URLs added\n";
         echo $newTitle, " Page Title";
