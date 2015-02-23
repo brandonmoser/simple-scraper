fc71cf3668967da99419a07cd92ce4acc59d49d5
Nick Andren <nick@trackif.com>
2015-02-23 07:33:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Scraper
+
+Simple Scraper is a very basic web crawler that is run from the command line. It is intended for programming practice using PHP.
+
+Usage
+-----
+
+> php scraper.php
---
+++ crawler.php
@@ -0,0 +1,38 @@
+<?php
+
+define('MAX_PAGES', 20);
+
+$url = 'http://www.tronixweb.com/';
+
+// List of URLs to be crawled
+$urlList = array(
+    $url
+);
+
+for($page=0; $page<count($urlList) && $page<MAX_PAGES; $page++) {
+    sleep(1);
+
+    echo $urlList[$page], "\n";
+
+    $content = @file_get_contents($urlList[$page]);
+    if (! $content) {
+        continue;
+    }
+
+    if (preg_match_all('@<a\b[^>]*?href="(https?://[^/"]*?\btronixweb\.com/[^>"]+)"[^>]*>@i', $content, $matches) != 0) {
+        $addedLinks = 0;
+
+        foreach($matches[1] as $newUrl) {
+            if (! in_array($newUrl, $urlList)) {
+                $urlList[] = $newUrl;
+                $addedLinks++;
+            }
+        }
+
+        echo $addedLinks, " new URLs added\n";
+    }
+
+    echo "\n";
+}
+
+print_r($urlList);
