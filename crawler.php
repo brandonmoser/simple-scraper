e7649fef92fb42e34a2c8c7da45275877933488e
Nick Andren <nick@trackif.com>
2015-02-24 07:55:14 -0600

    Fixed the breadcrumb checking mechanism, removed old comments

--- crawler.php
+++ crawler.php
@@ -32,19 +32,11 @@ for($page=0; $page<count($urlList) && $page<MAX_PAGES; $page++) {
         echo $addedLinks, " new URLs added\n";
     }
 
-    if (preg_match('@<div id="breadcrumbs">[^<a([\s\S]+?)]>([\s\S]+?)[^</a>]</div>@', $content, $matches) != 0) {
-    //this brings back the entire link and visible text of link: if (preg_match('@<div id="breadcrumbs">([\s\S]+?)</div>@', $content, $matches) != 0) {
-
-        echo $matches[1];
+    if (preg_match('@<div[^>]+?id="breadcrumbs"[^>]*>([\s\S]+?)</div>@i', $content, $matches) != 0) {
+        echo html_entity_decode(strip_tags($matches[1])), "\n";
     }
 
     echo "\n";
 }
 
 print_r($urlList);
-
-//on each pass, find breadcrumbs id and echo as plaintext to screen
-
-    //<div id="extra_links">
-    //    <div id="breadcrumbs"><a href="http://www.tronixweb.com/store/index.html">Home</a><span>&gt;</span><a href="http://www.tronixweb.com/store/3ds.html">Nintendo 2DS/3DS</a></div>
-    //</div>
