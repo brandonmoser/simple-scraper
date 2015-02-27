b87c35113df2212e77a8b883e6ff3a20e382491c
Justin Aune <aunjuse@dunwoody.edu>
2015-02-26 18:20:28 -0600

    Added italic styling to the innermost level of links and added some comments on what the recurse_hierarchy() does

--- crawler.php
+++ crawler.php
@@ -115,7 +115,8 @@ for($page=0; $page<count($urlList) && $page<$maxPage; $page++) {
     echo "\n\n";
 }
 
-file_put_contents('hierarchy.html', recurse_hierarchy($breadcrumbMap));
+//added 1 for the $level var in recurse_hierarchy()
+file_put_contents('hierarchy.html', recurse_hierarchy($breadcrumbMap, 1));
 
 // Extract the images that we found
 if (! empty($imageList)) {
@@ -148,6 +149,44 @@ if (! empty($imageList)) {
     echo "\n";
 }
 
+//function that generates the link list; $hierarchy is an array with info on the links and their visible names and $level sets the level of the lists, starting at 1 (see above)
+function recurse_hierarchy($hierarchy, $level) {  
+    //sets the default html_string output
+    $html_string = '';
+    //generates each link as part of a list by pulling the url and its visible name from the $hierarchy array
+    if (isset($hierarchy['url']) && isset($hierarchy['name'])) {
+        //once the list depth reaches 4, all links at this depth are italicized.  why 4 and not 3, no idea
+        if ($level == 4) { 
+            //generates li option with italic styling
+            $html_string = "\n<li style='font-style: italic;'><a href=\"".$hierarchy['url']."\">".$hierarchy['name']."</a>";
+        }
+        else { 
+            //generates li option with normal styling (seen before lvl 4(3))
+            $html_string = "\n<li><a href=\"".$hierarchy['url']."\">".$hierarchy['name']."</a>";
+        }
+    }
+    //generates the ul at each level as long as there's one or more links
+    if (isset($hierarchy['children']) && !empty($hierarchy['children'])) {
+        $html_string .= "\n<ul>";
+        //creates the visible links in the html document by having the function call itself to create a list and visible link to display, along with its level; this level is preserved through list generation for multiple items
+        foreach ($hierarchy['children'] as $child) {
+            $html_append = recurse_hierarchy($child, $level+1);
+            $html_string .= $html_append;
+            
+        }
+        //closes a ul
+        $html_string .= "\n</ul>";
+    }
+    //closes an li once its posted
+    if (isset($hierarchy['url']) && isset($hierarchy['name'])) {
+        $html_string .= "\n</li>";
+    }
+    //returns the completed list of links organized by hierarchy
+    return $html_string;
+}
+
+
+/*
 function recurse_hierarchy($hierarchy) {  
     $html_string = '';
     if (isset($hierarchy['url']) && isset($hierarchy['name'])) {
@@ -167,3 +206,4 @@ function recurse_hierarchy($hierarchy) {
     
     return $html_string;
 }
+*/
