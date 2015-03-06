b3a05dcd999adc4be069883d96b5cce4502a1b5c
Nick Andren <nick@trackif.com>
2015-03-06 17:39:58 -0600

    Updated check_cache() so that the first parameter is a reference to the cached data Added a debug message to display the current time Fetching the page for the first time is now coupled in with the rechecking mechanism (so all keys get a 0 unix time value if they don't exist, then are checked for expiration) Updated my_file_get_contents() to return HTTP headers Updated save_cache(), implemented load_cache() if(strpos() == 0) is now if(strpos() === 0) due to the common PHP pitfall of using it to check whether a substring exists or not
    Still have to actually handle the expires and cache-control headers because right now, they just advance the cache to the next 24 hours

--- check_cache.php
+++ check_cache.php
@@ -1,11 +1,11 @@
 <?php
 
+// Debug message
+echo "Right now, it's ", date('F j, Y, g:i A'), "\n\n";
+
 // hashmap of cached page expiration times
-$cacheTimes = array();
+$cacheTimes = load_cache();
 $testUrl = "http://www.tronixweb.com/";
-/* Test CODE. Delete this later!!! */
-date_default_timezone_set("UTC");
-/* END OF TEST CODE */
 
 // create cache folder
 if (! file_exists('cache')) {
@@ -15,55 +15,112 @@ if (! file_exists('cache')) {
     }
 }
 
-$test_page = check_cache($testUrl);
-save_cache();
+$test_page = check_cache($cacheTimes, $testUrl);
 
-function check_cache($url)
+function check_cache(&$cacheTimes, $url)
 {
     $key = md5($url);
-    $cacheTimes[$key] = 4000;
-    echo '$cacheTimes[$key] = '.$cacheTimes[$key];
-    if (true) {   //(isset($cacheTimes[$key])) {
-        if (time() > $cacheTimes[$key]) {  // expired, so must revalidate
-            $page = my_file_get_contents($url);
-            print("\nNew Expiration: ".strtotime("now + 24 hours")."\n");
-            // extract Expires value from headers
-            $headers = get_headers($url);
-            foreach ($headers as $header) {
-                if (strpos($header, "Expires") == 0) {
 
-                } elseif (strpos($header, "Cache-Control") == 0) {
-                
-                } else {
-                    echo "Cache time: ";
-                    $cacheTimes[$key] = strtotime("now + 24 hours");
-                }
+    // Determine whether or not we'll need to write the cache
+    $cacheChanged = false;
+
+    // Initialize the key if it's not set because our expiration mechanism will handle writing the cached file
+    if (! isset($cacheTimes[$key])) {
+        $cacheTimes[$key] = 0;
+    }
+
+    if (time() > $cacheTimes[$key]) {  // expired, so must revalidate
+        echo $key, ' expired on ', date('F j, Y, g:i A', $cacheTimes[$key]) , ", revalidating\n";
+        $response = my_file_get_contents($url, true); // Get the page, plus the response's HTTP headers
+
+        // extract Expires value from headers
+        list($header, $page) = explode("\r\n\r\n", $response, 2);
+
+        // Cache the target page
+        file_put_contents('cache/'.$key, $page);
+
+        // Get ready to step through each header line
+        $lines = explode("\r\n", $header);
+
+        // Step through each line of the header to determine the cache time
+        foreach ($lines as $line) {
+            if (strpos($line, 'Expires') === 0) {
+                echo "Expires detected\n";
+                $cacheTime = time() + (60 * 60 * 24); // Would use the Expires value here instead
+            } elseif (strpos($line, 'Cache-Control') === 0) {
+                echo "Cache-Control detected\n";
+                $cacheTime = time() + (60 * 60 * 24); // Would use the Cache-Control: max-age value here instead
+            } else {
+                $cacheTime = time() + (60 * 60 * 24); // Cache one day (60 seconds * 60 minutes * 24 hours)
             }
-        } else {
-            // load page from cache
-            $page = file_get_contents("cache/".$key); // should i be using my_file_get_contents() ?
+
+            if ($cacheTime > $cacheTimes[$key]) {
+                $cacheTimes[$key] = $cacheTime;
+                $cacheChanged = true;
+                echo 'Now expires on ', date('F j, Y, g:i A', $cacheTime), "\n";
+            }
         }
     } else {
-        // first time getting this page!
-        $page = my_file_get_contents($url);
-        file_put_contents("cache/".$key, $page);
+        // load page from cache
+        $page = file_get_contents("cache/".$key); // Don't need my_file_get_contents() here because we're grabbing a local file
     }
+
+    if ($cacheChanged) {
+        save_cache($cacheTimes);
+    }
+
     return $page;
 }
 
-function save_cache() {
-    file_put_contents("cacheTimes.txt", implode(",", $cacheTimes));
+function save_cache($cacheTimes)
+{
+    $output = '';
+    foreach($cacheTimes as $key => $value) {
+        $output .= $key . ',' . $value . "\n";
+    }
+
+    file_put_contents('cacheTimes.txt', $output);
 }
 
-function load_cache($cacheFile) {
+function load_cache()
+{
+    $cache = array();
 
+    $content = @file_get_contents('cacheTimes.txt');
+    if (empty($content)) {
+        echo "No cache file exists exists yet\n";
+        return $cache;
+    }
+
+    // Find all valid cache lines
+    if(preg_match_all('@^([^,]+),(\d+)$@m', $content, $matches) != 0) {
+        $keys   = $matches[1];
+        $values = $matches[2];
+
+        echo "Loaded cache successfully:\n";
+
+        // Populate the new cache
+        for($i=0; $i<count($keys); $i++) {
+            echo $keys[$i], ' expires on ', date('F j, Y, g:i A', $values[$i]), "\n";
+            $cache[$keys[$i]] = $values[$i];
+        }
+
+        echo "\n\n";
+    }
+
+    return $cache;
 }
 
-function my_file_get_contents($url) {
+function my_file_get_contents($url, $includeHeader=false)
+{
     $curl = curl_init();
 
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    if ($includeHeader) {
+        curl_setopt($curl, CURLOPT_HEADER, true);
+    }
 
     $content = curl_exec($curl);
     curl_close($curl);
