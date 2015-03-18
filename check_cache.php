f36bee51297c2c4f52bfe38a25715e19a60a6edd
Joshua Zemke <zemke019@umn.edu>
2015-03-18 13:36:38 -0500

    fixed preg_match statements, switched if Expires and Cache-control if clauses to accurately reflect Cache-control priority, fixed regexes

--- check_cache.php
+++ check_cache.php
@@ -44,14 +44,15 @@ function check_cache(&$cacheTimes, $url)
 
         // Step through each line of the header to determine the cache time
         foreach ($lines as $line) {
-            if (strpos($line, 'Cache-Control') === 0) {
+            if (stripos($line, 'Cache-Control') === 0) {
                 echo "Cache-Control detected\n";
-                // same question as below case, but if preg_match fails here will the end result be the same as max-age=0 ?
-                $maxAge = preg_match('@max-age=([0-9]+)\,@', $line); 
+                preg_match('@max-age=(\d+),@', $line, $match);
+                $maxAge = $match; 
                 $cacheTime = time() + (int) $maxAge; // current time + number of seconds in max-age
-            } elseif (strpos($line, 'Expires') === 0) {
+            } elseif (stripos($line, 'Expires') === 0) {
                 echo "Expires detected\n";
-                $expireTime = preg_match('@Expires:([\s\S]+)$@', $line); // do I need a case for if the preg_match fails?
+                preg_match('@Expires:(.+)$@', $line, $match);
+                $expireTime = $match;
                 $cacheTime = strtotime($expireTime);
             } else {
                 $cacheTime = time() + (60 * 60 * 24); // Cache one day (60 seconds * 60 minutes * 24 hours)
