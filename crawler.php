125fd2eacfcc1c328900c5874b5e5a9b4797c888
Joshua Zemke <zemke019@umn.edu>
2015-03-18 13:57:57 -0500

    added check_cache(), save_cache(), load_cache() to crawler

--- crawler.php
+++ crawler.php
@@ -44,6 +44,8 @@ if (! file_exists('cache')) {
     }
 }
 
+$cacheTimes = load_cache();
+
 // List of product images that we found on this site
 $imageList = array();
 
@@ -205,4 +207,105 @@ function recurse_hierarchy($hierarchy, $level)
     }
     //returns the completed list of links organized by hierarchy
     return $html_string;
+}
+
+// gets $url response and references the $cacheTimes hashmap to see if we need to revalidate
+function check_cache(&$cacheTimes, $url)
+{
+    $key = md5($url);
+
+    // Determine whether or not we'll need to write the cache
+    $cacheChanged = false;
+
+    // Initialize the key if it's not set because our expiration mechanism will handle writing the cached file
+    if (! isset($cacheTimes[$key])) {
+        $cacheTimes[$key] = 0;
+    }
+
+    if (time() > $cacheTimes[$key]) {  // expired, so must revalidate
+        echo $key, ' expired on ', date('F j, Y, g:i A', $cacheTimes[$key]) , ", revalidating\n";
+        $response = my_file_get_contents($url, true); // Get the page, plus the response's HTTP headers
+
+        // extract Expires value from headers
+        list($header, $page) = explode("\r\n\r\n", $response, 2);
+
+        // Cache the target page
+        file_put_contents('cache/'.$key, $page);
+
+        // Get ready to step through each header line
+        $lines = explode("\r\n", $header);
+
+        // Step through each line of the header to determine the cache time
+        foreach ($lines as $line) {
+            if (stripos($line, 'Cache-Control') === 0) {
+                echo "Cache-Control detected\n";
+                preg_match('@max-age=(\d+),@', $line, $match);
+                $maxAge = $match; 
+                $cacheTime = time() + (int) $maxAge; // current time + number of seconds in max-age
+            } elseif (stripos($line, 'Expires') === 0) {
+                echo "Expires detected\n";
+                preg_match('@Expires:(.+)$@', $line, $match);
+                $expireTime = $match;
+                $cacheTime = strtotime($expireTime);
+            } else {
+                $cacheTime = time() + (60 * 60 * 24); // Cache one day (60 seconds * 60 minutes * 24 hours)
+            }
+
+            if ($cacheTime > $cacheTimes[$key]) {
+                $cacheTimes[$key] = $cacheTime;
+                $cacheChanged = true;
+                echo 'Now expires on ', date('F j, Y, g:i A', $cacheTime), "\n";
+            }
+        }
+    } else {
+        // load page from cache
+        $page = file_get_contents("cache/".$key); // Don't need my_file_get_contents() here because we're grabbing a local file
+    }
+
+    if ($cacheChanged) {
+        save_cache($cacheTimes);
+    }
+
+    return $page;
+}
+
+// populates .txt file with hashmap key-values
+function save_cache($cacheTimes)
+{
+    $output = '';
+    foreach($cacheTimes as $key => $value) {
+        $output .= $key . ',' . $value . "\n";
+    }
+
+    file_put_contents('cacheTimes.txt', $output);
+}
+
+// extracts hashmap key-values from .txt file
+function load_cache()
+{
+    $cache = array();
+
+    $content = @file_get_contents('cacheTimes.txt');
+    if (empty($content)) {
+        echo "No cache file exists exists yet\n";
+        return $cache;
+    }
+
+    // Find all valid cache lines
+    if(preg_match_all('@^([^,]+),(\d+)$@m', $content, $matches) != 0) {
+        $keys   = $matches[1];
+        $values = $matches[2];
+
+        echo "Loaded cache successfully:\n";
+
+        // Populate the new cache
+        for($i=0; $i<count($keys); $i++) {
+            echo $keys[$i], ' expires on ', date('F j, Y, g:i A', $values[$i]), "\n";
+            $cache[$keys[$i]] = $values[$i];
+        }
+
+        echo "\n\n";
+    }
+
+    return $cache;
 }
